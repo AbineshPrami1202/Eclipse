dd1623697a07c18814fbaa44083cc682a29f7854
AbineshPrami1202 <abinesh3292@gmail.com>
2024-02-22 17:13:21 +0530

    done

---
+++ src/org/packagename/Abinesh.java
@@ -0,0 +1,22 @@
+package org.packagename;
+
+public class Abinesh {
+	public static void main(String[] args) {
+		int age = 28;
+				System.out.println("\nEmp age:" + 32);
+	
+	   long salary = 45000;
+	   
+	System.out.println("\nEmp salary:" + 45000);
+	short average = 7867;
+	System.out.println("\nEmp average" + 7867);
+	long phnum = 2345667;
+	System.out.println("\nEmp phnum"+ 2345667);
+	char gender = 'f';
+	System.out.println("\nEmp gender" +'f');
+	String name = "smartskill@gmail.com";
+			System.out.println("\nEmp Name:"+ name);
+
+	}
+
+}
